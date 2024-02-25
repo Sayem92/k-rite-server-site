0d807167f9d2b46876c8f4ea4cbbaa859451bd02
Sayem92 <sayemmiha123@gmail.com>
2024-02-25 19:44:25 +0600

    add details api

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ async function run() {
     // get task data for data---
     app.get("/updateTask/:id", async (req, res) => {
       const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await addTaskCollection.findOne(query);
+      res.send(result);
+    });
+
+    // get my task details ---
+    app.get("/details/:id", async (req, res) => {
+      const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await addTaskCollection.findOne(query);
       res.send(result);
